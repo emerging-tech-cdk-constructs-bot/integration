e84deac949bed9da5c4c99396596e9196a5d361e
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-05-14 10:00:30 -0700

    if no signature fail

--- lib/github-app.function.ts
+++ lib/github-app.function.ts
@@ -2,27 +2,23 @@ import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 import { App, Octokit } from 'octokit';
 
 // https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries
-// import * as crypto from 'crypto';
+import * as crypto from 'crypto';
 
-// const WEBHOOK_SECRET: string = process.env.WEBHOOK_SECRET;
+const GITHUB_APP_WEBHOOK_SECRET: string = String(process.env.GITHUB_APP_WEBHOOK_SECRET);
+const X_HUB_SIGNATURE_256 = "X-Hub-Signature-256";
 
-// const verify_signature = (req: Request) => {
-//   const signature = crypto
-//     .createHmac("sha256", WEBHOOK_SECRET)
-//     .update(JSON.stringify(req.body))
-//     .digest("hex");
-//   let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
-//   let untrusted =  Buffer.from(req.headers.get("x-hub-signature-256"), 'ascii');
-//   return crypto.timingSafeEqual(trusted, untrusted);
-// };
-
-// const handleWebhook = (req: Request, res: Response) => {
-//   if (!verify_signature(req)) {
-//     res.status(401).send("Unauthorized");
-//     return;
-//   }
-//   // The rest of your logic here
-// };
+const verify_signature = (event: APIGatewayEvent) => {
+    if ((event.headers[X_HUB_SIGNATURE_256] === undefined) || (event.headers[X_HUB_SIGNATURE_256] === null)) {
+        return false;
+    }
+    const signature = crypto
+        .createHmac("sha256", GITHUB_APP_WEBHOOK_SECRET)
+        .update(JSON.stringify(event.body))
+        .digest("hex");
+    let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+    let untrusted =  Buffer.from(event.headers[X_HUB_SIGNATURE_256]!, 'ascii');
+    return crypto.timingSafeEqual(trusted, untrusted);
+};
 
 const app = new App({
     appId: String(process.env.GITHUB_APP_ID),
@@ -36,8 +32,22 @@ const octokit = new Octokit({
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+    if (!verify_signature(event)) {
+        return {
+            statusCode: 401,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                message: 'Unauthorized: Unable to verify signature',
+            }),
+        };
+    }
     return {
         statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
         body: JSON.stringify({
             message: 'github app',
         }),
