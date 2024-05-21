ffa1a6af4d570d5ef17282d9971b80ad7d2f5032
Scott Schreckengaust <scottschreckengaust@users.noreply.github.com>
2024-05-20 17:16:00 -0700

    attempt to restrict by ip addresses

--- lib/github-app-construct.ts
+++ lib/github-app-construct.ts
@@ -65,6 +65,27 @@ export class GitHubAppConstruct extends Construct {
     });
     const githubAppApiGateway = new LambdaRestApi(this, 'apigateway', {
       handler: githubAppFunction,
+      policy: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            actions: ['execute-api:Invoke'],
+            principals: [new iam.AnyPrincipal()],
+            resources: ['execute-api:/*/*/*'],
+          }),
+          new iam.PolicyStatement({
+            effect: iam.Effect.DENY,
+            principals: [new iam.AnyPrincipal()],
+            actions: ['execute-api:Invoke'],
+            resources: ['execute-api:/*/*/*'],
+            conditions: {
+              NotIpAddress: {
+                // gh api graphql --field query='query{meta {hookIpAddresses}}' | jq -c '.data.meta.hookIpAddresses'
+                'aws:SourceIp': ["192.30.252.0/22","185.199.108.0/22","140.82.112.0/20","143.55.64.0/20","2a0a:a440::/29","2606:50c0::/32"],
+              },
+            },
+          }),
+        ],
+      }),
     });
   }
 }
